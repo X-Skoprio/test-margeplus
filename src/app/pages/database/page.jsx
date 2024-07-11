307985ad45eb7cfe08ba0f2cf11067007c532592
X-Skoprio <61594440+X-Skoprio@users.noreply.github.com>
2024-07-11 08:45:28 +0200

    test

--- src/app/pages/database/page.jsx
+++ src/app/pages/database/page.jsx
@@ -22,7 +22,7 @@ const RecipesPage = async () => {
     <div className='grid place-items-center w-[50%]'>
       <h1>Recipes</h1>
       {recipes.map((recipe) => (
-        <div key={recipe.RecipeID} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
+        <div key={recipe.RecipeID} style={{ w: '100%', border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
           <h2>{recipe.RecipeLabel}</h2>
           <p><strong>Type:</strong> {recipe.Type}</p>
           <p><strong>Portion Price:</strong> {recipe.PortionPrice}</p>
