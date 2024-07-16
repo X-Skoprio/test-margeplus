66f76d54413211b25f78cf6a35614f717a874154
X-Skoprio <61594440+X-Skoprio@users.noreply.github.com>
2024-07-16 16:24:50 +0200

    functional selection app

--- src/app/pages/recipes/page.js
+++ src/app/pages/recipes/page.js
@@ -3,8 +3,11 @@ import { useEffect, useState } from "react";
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
+  const [updatedRecipes, setUpdatedRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [isLoadingImageClick, setIsLoadingImageClick] = useState(false); // New state for loading image click
+///une fois recette à la fois 
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,32 +17,29 @@ const Recipes = () => {
           throw new Error("Network response was not ok");
         }
         const jsonData = await response.json();
-
-        // Convert the object into an array of recipes
+  
         const recipesArray = Object.keys(jsonData).map((key) => ({
           RecipeID: key,
           ...jsonData[key],
         }));
-
-        // Filter recipes based on conditions
+  
         const filteredRecipes = recipesArray.filter(
           (recipe) =>
             recipe.ImageLink !== "" &&
             recipe.Validated &&
             recipe.SelectedImageButton === ""
         );
-
-        // Fetch image URLs for each recipe
+  
         const recipesWithImages = await Promise.all(
           filteredRecipes.map(async (recipe) => {
             const recipeId = recipe.RecipeID;
             const imageUrls = await Promise.all(
               [1, 2, 3, 4].map(async (number) => {
                 try {
-                  const imageUrl = `/promptImages/${recipeId}_${number}.png`; // Adjust file extension as per your images
+                  const imageUrl = `/promptImages/${recipeId}_${number}.png`;
                   const response = await fetch(imageUrl);
                   if (!response.ok) {
-                    return null; // Image not found or error fetching
+                    return null;
                   }
                   return imageUrl;
                 } catch (error) {
@@ -51,20 +51,23 @@ const Recipes = () => {
             return { ...recipe, imageUrls };
           })
         );
-
+  
+        console.log("Recipes with images:", recipesWithImages); // Log recipes with images
         setRecipes(recipesWithImages);
       } catch (error) {
+        console.error("Error fetching recipes:", error);
         setError(error);
       } finally {
         setLoading(false);
       }
     };
-
+  
     fetchData();
-  }, []);
-
+  }, [recipes]);
+  
   const handleImageClick = async (recipeId, imageIndex) => {
     try {
+      setLoading(true);
       const response = await fetch(`/api/recipes`, {
         method: "POST",
         headers: {
@@ -73,99 +76,123 @@ const Recipes = () => {
         body: JSON.stringify({
           RecipeID: recipeId,
           SelectedImageButton: `U${imageIndex}`,
-        }), // Adjusted index
+        }),
       });
-
       if (!response.ok) {
         throw new Error("Failed to update SelectedImageButton");
       }
+  
+      const jsonData = await response.json();
+  
+      const recipesArray = Object.keys(jsonData).map((key) => ({
+        RecipeID: key,
+        ...jsonData[key],
+      }));
 
-      const updatedRecipes = await response.json();
-      setRecipes(updatedRecipes); // Update state with the updated recipes
+      const filteredRecipes = recipesArray.filter(
+        (recipe) =>
+          recipe.ImageLink !== "" &&
+          recipe.Validated &&
+          recipe.SelectedImageButton === ""
+      );
+
+      const recipesWithImages = await Promise.all(
+        filteredRecipes.map(async (recipe) => {
+          const recipeId = recipe.RecipeID;
+          const imageUrls = await Promise.all(
+            [1, 2, 3, 4].map(async (number) => {
+              try {
+                const imageUrl = `/promptImages/${recipeId}_${number}.png`;
+                const response = await fetch(imageUrl);
+                if (!response.ok) {
+                  return null;
+                }
+                return imageUrl;
+              } catch (error) {
+                console.error(`Error fetching image ${imageUrl}:`, error);
+                return null;
+              }
+            })
+          );
+          return { ...recipe, imageUrls };
+        })
+      );
+
+      console.log("Recipes with images:", recipesWithImages); // Log recipes with images
+      setRecipes(recipesWithImages);
     } catch (error) {
       console.error("Error updating SelectedImageButton:", error);
       // Handle error as needed
     }
   };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
+  
+  
   return (
     <div className="w-full h-svh relative">
       <img src="/images/margeplus_logo_preview_rev_1.png" className="absolute w-36 left-16 top-5"></img>
-      {recipes.length === 0 ? (
-        <p>No recipes found that match the criteria.</p>
-      ) : (
+  
+    
         <ul>
-          {recipes.map((recipe) => (
-            <li key={recipe.RecipeID}>
-              <div className="w-full flex justify-center items-center">
-                <p className="font-bold h-full flex items-center justify-center mt-16 pr-5">
-                  ID: {recipe.RecipeID}
-                </p>
-                <p className="w-[50%] flex items-center justify-center font-bold text-3xl border-t-2 border-b-2 border-primary mt-16">
-                  {recipe.RecipeLabel}
-                </p>
-              </div>
-              <div className="">
-                <div className="grid grid-rows-2-2">
-                  <div className="">
-                    
-             
-
-                  
-                    <ul className="p- flex justify-center items-center space-x-6 w-full mt-24">
-                    <p className="font-bold ml-4">Ingredients:</p>
-                      {recipe.Ingredients.map((ingredient, index) => (
-                        <li key={index} className="">
-                          <p>{ingredient["Produit"]}</p>
-                        </li>
-                      ))}
-                    </ul>
-              
-                  </div>
-                  <div className="grid grid-cols-4 gap-4 col-span-1 px-16 py-8">
-                    {recipe.imageUrls.map((imageUrl, index) => (
-                      <div key={index} className="flex justify-start items-start">
-                        <img
-                          src={imageUrl}
-                          alt={`Recipe ${recipe.RecipeID} Image ${index + 1}`}
-                          onClick={() =>
-                            handleImageClick(recipe.RecipeID, index + 1)
-                          }
-                          className="cursor-pointer mb-4"
-                        />
-                      </div>
-                    ))}
-                  </div>
-                </div>
-                <div className="w-full flex justify-center space-x-8 mb-36">
-                <button
-                  className="btn-primary"
-                  onClick={() => handleImageClick(recipe.RecipeID, "R")}
-                >
-                  Regéner 4 images
-                </button>
-                <button
-                  className="btn-primary"
-                  onClick={() => handleImageClick(recipe.RecipeID, "RR")}
-                >
-                  Regéner 4 images avec REF
-                </button>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+          {!loading && <>
+            {recipes.map((recipe, index) => (
+  <li key={recipe.RecipeID} className={`${index != 0 ? "hidden" : ""}`}>
+    <div className="w-full flex justify-center items-center">
+      <p className={`font-bold h-full flex items-center justify-center mt-16 pr-5`}>
+        ID: {recipe.RecipeID}
+      </p>
+      <p className={`w-[50%] flex items-center justify-center font-bold text-3xl border-t-2 border-b-2 border-primary mt-16 py-1 `}>
+        {recipe.RecipeLabel}
+      </p>
     </div>
-  );
+    <div className="">
+      <div className="grid grid-rows-2-2">
+        <div className="">
+          <ul className="flex justify-center items-center space-x-6 w-full mt-24">
+            <p className="font-bold ml-4">Ingredients :</p>
+            {recipe.Ingredients.map((ingredient, index) => (
+              <li key={index} className="">
+                <p>{ingredient["Produit"]}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="grid grid-cols-4 gap-4 col-span-1 px-16 py-8">
+          {recipe.imageUrls.map((imageUrl, index) => (
+            <div key={index} className="flex justify-start items-start">
+              <img
+                src={imageUrl}
+                alt={`Recipe ${recipe.RecipeID} Image ${index + 1}`}
+                onClick={() => handleImageClick(recipe.RecipeID, index + 1)}
+                className="cursor-pointer mb-4"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full flex justify-center space-x-8 mb-36">
+        <button
+          className="btn-primary"
+          onClick={() => handleImageClick(recipe.RecipeID, "R")}
+          disabled={isLoadingImageClick} // Disable button while loading
+        >
+          Regéner 4 images
+        </button>
+        <button
+          className="btn-primary"
+          onClick={() => handleImageClick(recipe.RecipeID, "RR")}
+          disabled={isLoadingImageClick} // Disable button while loading
+        >
+          Regéner 4 images avec REF
+        </button>
+      </div>
+    </div>
+  </li>
+))}
+</>}
+        </ul>
+    </div>
+  
+);
 };
 
 export default Recipes;
